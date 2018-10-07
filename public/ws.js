3f9a3940ec53e144b574deb87f26fc43d024a7c5
A.Olokhtonov <a.olokhtonov@norsi-trans.ru>
2018-10-07 15:45:19 +0300

    Раздача истории чатов при подключении

--- public/ws.js
+++ public/ws.js
@@ -1,5 +1,5 @@
 window.addEventListener('load', () => {
-  var exampleSocket = new WebSocket("ws://localhost:3000/chat")
+  var exampleSocket = new WebSocket("ws://localhost:3000/")
 
   exampleSocket.onopen = (event) => {
     exampleSocket.send("hello " + Math.random())
